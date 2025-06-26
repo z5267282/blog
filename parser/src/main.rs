eab9a0dbedf9f1a7a518050dbc174e98b83a3843
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-06-26 15:06:47 +1000

    starting parsing of title

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,36 @@
+enum HTMLElement {
+    Title(String),
+    // level, text
+    Header(usize, String),
+    // prose
+    Paragraph(Vec<String>),
+    // language, lines
+    Code(String, Vec<String>),
+}
+
+fn parse_title(line: &String) -> Option<(usize, String)> {
+    for curr in line.char_indices() {
+        match curr {
+            None => break,
+            Some(idx, letter) => {
+                match letter {
+                    '#' => continue,
+                    _ => break,
+                }
+            }
+        }
+    }
+    todo!()
+}
+
+fn parse_markdown(text: &Vec<String>) -> Vec<HTMLElement> {
+    let mut answer: Vec<HTMLElement> = Vec::new();
+    for line in text {
+        if (line.fi)
+    }
+    answer
+}
+
 fn main() {
     println!("Hello, world!");
 }
