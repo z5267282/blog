fb9626ca42a830f5e4ad6132a52e794d6adc5f16
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-07-18 13:34:09 +1000

    nearly can dump file

--- parser/src/parse/dump_file.rs
+++ parser/src/parse/dump_file.rs
@@ -1,6 +1,6 @@
-use std::fs::{self, create_dir, exists, read_dir, remove_dir, File};
-use std::io::{Read, Result};
-use std::path::PathBuf;
+use std::fs::{create_dir, exists, read_dir, read_to_string, remove_dir, File};
+use std::io::Read;
+use std::path::{Path, PathBuf};
 
 use serde_json::to_string_pretty;
 
@@ -8,31 +8,70 @@ use crate::parse::html_element::HTMLElement;
 use crate::parse::paths::{JSON, MARKDOWN};
 use crate::parse::to_html::parse_markdown;
 
-pub fn dump_blogs() -> () {
-    for lang in fs::read_dir(MARKDOWN).unwrap() {
-        let mut dump_path = PathBuf::from(JSON);
-        let name = lang.unwrap().path().file_name().unwrap();
-        dump_path.push(name);
-        // clear out the old json
-        if exists(dump_path).is_ok() {
-            println!("deleting old folder: {}", dump_path.display());
-            remove_dir(dump_path);
+pub fn dump_blogs() -> Result<(), std::io::Error> {
+    for try_lang in read_dir(MARKDOWN)? {
+        let lang = try_lang?.path();
+        if !lang.is_dir() {
+            continue;
         }
-        println!("reset folder: {}", dump_path.display());
-        create_dir(dump_path);
-        for blog in read_dir(lang.unwrap().path()) {}
+        let mut read_path = PathBuf::from(MARKDOWN);
+        let language = lang.file_name().ok_or(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "basename could not be extracted from absolute path {}",
+                read_path.display()
+            ),
+        ))?;
+        read_path.push(language);
+        // delete dump folder if it exists
+        if exists(&read_path).is_ok() {
+            remove_dir(&read_path)?;
+        }
+        create_dir(&read_path)?;
+
+        for blog in read_dir(&read_path)? {
+            let entry = blog?.path();
+            let contents = dump_to_str(&read_path)?;
+            let mut dump_path = PathBuf::from(JSON);
+            dump_path.push(language);
+            let json_name = lang
+                .file_name()
+                .ok_or(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    format!(
+                        "basename could not be extracted from absolute path {}",
+                        read_path.display()
+                    ),
+                ))?
+                .to_str()
+                .ok_or(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "could not convert file name string into UTF-8 for path {}",
+                ))?
+                .trim_end_matches(".json")
+                .to_string()
+                + ".md";
+            dump_path.push(json_name);
+            // let mut file = File::open(path)
+        }
     }
+    Ok(())
 }
 
-fn dump_to_json(path: &str) -> () {
-    let mut file = File::open("../blog/lang/shell/background-fg.md").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let markdown = contents
+fn dump_to_str(path: &PathBuf) -> Result<String, std::io::Error> {
+    let markdown = read_to_string(path)?
         .lines()
         .into_iter()
         .map(|s| s.to_string())
         .collect::<Vec<String>>();
     let json = parse_markdown(&markdown);
-    let jsonified = to_string_pretty::<Vec<HTMLElement>>(&json).unwrap();
+    to_string_pretty::<Vec<HTMLElement>>(&json).map_err(|_| {
+        std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "Serde error occurred when serialising parsed data in {}",
+                path.display()
+            ),
+        )
+    })
 }
---
+++ parser/src/parse/paths.rs
@@ -0,0 +1,4 @@
+/// Paths for dump files relative to the target directory.
+
+pub const MARKDOWN: &str = "../blog/lang";
+pub const JSON: &str = "../blog/lang-json-dump";
