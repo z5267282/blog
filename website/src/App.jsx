5ca90555005a970e7d794a3a5659fc2cbb9662f6
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-07-24 16:41:38 +1000

    routing seems to work

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -5,15 +5,23 @@ import BlogHub from "./pages/BlogHub";
 import Projects from "./pages/Projects";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LanguageHub from "./pages/LanguageHub";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AboutMe />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/blogs" element={<BlogHub />}></Route>
-        <Route path="/*" element={<div>not found :&#40;</div>}></Route>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blogs">
+          <Route index element={<BlogHub />} />
+          <Route path=":lang">
+            <Route index element={<LanguageHub />} />
+            <Route path=":title" element={<Blog />} />
+          </Route>
+        </Route>
+        <Route path="/*" element={<div>not found :&#40;</div>} />
       </Routes>
     </BrowserRouter>
   );
