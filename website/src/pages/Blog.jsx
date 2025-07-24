440c15ddeb45d2423dd1049a05809678ab401463
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-07-24 16:51:14 +1000

    language routing works

--- website/src/pages/Blog.jsx
+++ website/src/pages/Blog.jsx
@@ -5,3 +5,62 @@ export default function Blog() {
 
   return <div>blog {title}</div>;
 }
+
+const genHTML = (htmlData) => {
+  console.log(htmlData);
+  switch (htmlData.type) {
+    case "Header": {
+      const level = htmlData.level;
+      const content = htmlData.content;
+      switch (level) {
+        case 1:
+          return <h1>{content}</h1>;
+        case 2:
+          return <h2>{content}</h2>;
+        case 3:
+          return <h3>{content}</h3>;
+        case 4:
+          return <h4>{content}</h4>;
+        case 5:
+          return <h5>{content}</h5>;
+        default:
+          return <h6>{content}</h6>;
+      }
+    }
+    case "Code": {
+      const code = htmlData.code;
+      return (
+        <pre>
+          <code>{code.join("\n")}</code>
+        </pre>
+      );
+    }
+    case "OrderedList": {
+      const list = htmlData.list;
+      console.log(list);
+      return (
+        <ol>
+          {list.map((li) => (
+            <li>{li}</li>
+          ))}
+        </ol>
+      );
+    }
+    case "UnorderedList": {
+      const list = htmlData.list;
+      return (
+        <ul>
+          {list.map((li) => (
+            <li>{li}</li>
+          ))}
+        </ul>
+      );
+    }
+    case "Paragraph": {
+      const lines = htmlData.lines;
+      return <p>{lines.join(" ")}</p>;
+    }
+    default:
+      return <p>ERROR: unsupported HTML type {htmlData.type}</p>;
+  }
+};
--- website/src/pages/BlogHub.jsx
+++ website/src/pages/BlogHub.jsx
@@ -1,84 +1,18 @@
+import { Link } from "react-router-dom";
+
 import blogs from "../blog-lang.json";
 
 export default function BlogHub() {
   return (
-    <div>
+    <ul>
       {blogs.map((lang) => {
-        const { language, blogs } = lang;
+        const { language } = lang;
         return (
-          <>
-            <h1>{language}</h1>
-            {blogs.map((blog) => {
-              console.log(blog.html);
-              return (
-                <>
-                  <h2>{blog.title}</h2>
-                  <div>{blog.html.map((h) => genHTML(h))}</div>
-                </>
-              );
-            })}
-          </>
+          <li>
+            <Link to={`/blogs/${language}`}>{language}</Link>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 }
-
-const genHTML = (htmlData) => {
-  console.log(htmlData);
-  switch (htmlData.type) {
-    case "Header": {
-      const level = htmlData.level;
-      const content = htmlData.content;
-      switch (level) {
-        case 1:
-          return <h1>{content}</h1>;
-        case 2:
-          return <h2>{content}</h2>;
-        case 3:
-          return <h3>{content}</h3>;
-        case 4:
-          return <h4>{content}</h4>;
-        case 5:
-          return <h5>{content}</h5>;
-        default:
-          return <h6>{content}</h6>;
-      }
-    }
-    case "Code": {
-      const code = htmlData.code;
-      return (
-        <pre>
-          <code>{code.join("\n")}</code>
-        </pre>
-      );
-    }
-    case "OrderedList": {
-      const list = htmlData.list;
-      console.log(list);
-      return (
-        <ol>
-          {list.map((li) => (
-            <li>{li}</li>
-          ))}
-        </ol>
-      );
-    }
-    case "UnorderedList": {
-      const list = htmlData.list;
-      return (
-        <ul>
-          {list.map((li) => (
-            <li>{li}</li>
-          ))}
-        </ul>
-      );
-    }
-    case "Paragraph": {
-      const lines = htmlData.lines;
-      return <p>{lines.join(" ")}</p>;
-    }
-    default:
-      return <p>ERROR: unsupported HTML type {htmlData.type}</p>;
-  }
-};
