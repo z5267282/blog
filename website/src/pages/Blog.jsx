e09990431b6f898d716d8f2cceb4535518323d61
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-08-01 13:07:00 +1000

    title works now

--- website/src/pages/Blog.jsx
+++ website/src/pages/Blog.jsx
@@ -8,7 +8,8 @@ export default function Blog() {
 
   return (
     <>
-      <h1>blog {title}</h1>
+      <title>{title}</title>
+      <header>blog {title}</header>
       {getBlog(lang, title).map((html) => genHTML(html))}
     </>
   );
