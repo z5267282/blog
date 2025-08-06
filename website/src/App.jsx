13b449c3aec21559d9b41aa27f1e8b166fa7b800
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-08-06 15:54:42 +1000

    need to refactor where main content is - must be inside BrowserRouter

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -12,28 +12,35 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LanguageHub from "./pages/LanguageHub";
 import Blog from "./pages/Blog";
 
-function App() {
+export default function App() {
   return (
-    <div className="bg-red-50 ml-[10%] mr-[10%]">
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/">
-            <Route index element={<AboutMe />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="blogs">
-              <Route index element={<BlogHub />} />
-              <Route path=":lang">
-                <Route index element={<LanguageHub />} />
-                <Route path=":title" element={<Blog />} />
-              </Route>
-            </Route>
-            <Route path="*" element={<div>not found :&#40;</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    // the background should be as big as wide as the screen, and at least as tall as the screen where scrolling is allowed.
+    <div className="w-screen min-h-screen bg-[#fff4e2] overflow-auto">
+      <div className="bg-white ml-[20%] mr-[20%] min-h-screen">
+        <DynamicContent />
+      </div>
     </div>
   );
 }
 
-export default App;
+function DynamicContent() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<AboutMe />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="blogs">
+            <Route index element={<BlogHub />} />
+            <Route path=":lang">
+              <Route index element={<LanguageHub />} />
+              <Route path=":title" element={<Blog />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<div>not found :&#40;</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
