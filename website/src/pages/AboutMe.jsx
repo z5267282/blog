ba89d05f4bd1728cc28b7aecffc8bee7bf6b4d5f
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-08-07 20:08:28 +1000

    about me just needs links now

--- website/src/pages/AboutMe.jsx
+++ website/src/pages/AboutMe.jsx
@@ -1,33 +1,82 @@
-import "react";
-
 export default function AboutMe() {
   return (
-    <div>
-      <img src="https://cvws.icloud-content.com/B/ARJrjqSZAqHBfFwfX7hWkXxHpfkxAWTAsQgBOAgzAS32ksCuFA9zyxgc/display.jpg?o=AnPrlp50ngFifJ1udA-eKJe2qQkDz4CViVBcdT9T4FyT&v=1&x=3&a=CAogqiMjW0J2nyTGRuZnAyYD--uQb2c-7zo8NWMeyJjYmXISbxDf0a_VgjMY366L14IzIgEAUgRHpfkxWgRzyxgcaidS6xvdpyirVneSpBn6FTf555mZO5c8coyMTfQQIc6hbJ3SLmJVkiZyJ7phwpGMc598LsKQy7tOXKhOOLhDBJa0ZTMg8QZ0ZGRaKL_2p-uOEA&e=1753066166&fl=&r=46e925b2-7699-4919-b3e1-7331423518cc-1&k=5q8GL5CHufZxsnuzh39a7Q&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=142&s=VGzAdZflr7RqS3BVmEOUCI-J6zI&cd=i" alt="display picture" />
-      <p>Who am I?</p>
-      <ul>
-        <li>UNSW Software Engineering Graduate &#40;2024 December&#41;</li>
-        <li>Passionate programmer</li>
-      </ul>
-      <p>What I like</p>
-      <ul>
-        <li>Scripting</li>
-        <li>Automation</li>
-      </ul>
-      <p>Programming languages I developed in</p>
-      <ul>
-        <li>Objected-Oriented: C/C++, Java, Python</li>
-        <li>Scripting: Shell scripting, Perl</li>
-        <li>
-          Web Front-End: ReactJS, Tailwind CSS, Vanilla JavaScript, TypeScript
-        </li>
-      </ul>
-      <p>
-        Contact:&#32;
-        <a href="mailto:sunny.chen1@unswalumni.com">
-          sunny.chen1@unswalumni.com
-        </a>
-      </p>
+    <div className="pt-10 px-10">
+      <div className="grid grid-cols-[25%_70%] gap-x-10">
+        <img
+          // format from here: https://stackoverflow.com/questions/10311092/displaying-files-e-g-images-stored-in-google-drive-on-a-website
+          src="https://lh3.googleusercontent.com/d/1pO_ty9Cs7ZkRTOOrmRTa_lzkxgPWrVfU"
+          // this is needed otherwise we get a 429 status code: https://stackoverflow.com/questions/79052869/google-drive-returns-429-when-using-saved-photo-as-src-for-img-tag
+          referrerPolicy="no-referrer"
+          alt="display picture"
+          width="200"
+          height="200"
+          className="inline"
+        />
+        <span className="flex justify-center items-center">
+          <div className="h-auto w-auto">
+            <Header1 content="About Me" />
+            <p className="mt-5">
+              Hi, I'm Sunny. I graduated from Software Engineering at UNSW in
+              December of 2024. I'm currently a course developer and tutor for
+              the Advanced C++ Programming Course. This website showcases some
+              of my coding projects and blogs. Enjoy!
+            </p>
+          </div>
+        </span>
+      </div>
+      <div className="mt-7.5">
+        <Header1 content="What I've Worked On" />
+        <p>
+          I've done fullstack development for various early-stage startups with
+          B2B SAS products in areas ranging from accounting to project
+          management. This involved working closely with founders to create
+          features from inception to implementation - i.e. from mockup to a
+          staging environment release.
+        </p>
+      </div>
+      <div className="mt-5">
+        <Header1 content="Technical Expertise" />
+        <p>
+          I love learning new languages and technologies. Here is a
+          comprehensive list of the ones I've learnt over my programming
+          journey.
+        </p>
+        <ol>
+          <ListItemWithBoldPrefix
+            bold="General-Purpose Languages"
+            normal="C, Java, C++, Python, Rust"
+          />
+          <ListItemWithBoldPrefix
+            bold="Scripting"
+            normal="Bash, Powershell, Perl"
+          />
+          <ListItemWithBoldPrefix
+            bold="Front-End Development"
+            normal="HTML, CSS, Vanilla JS, Typescript, React JS, Tailwind CSS"
+          />
+        </ol>
+      </div>
+      <div className="mt-5">
+        <Header1 content="Links" />
+        <p>
+          Contact:&#32;
+          <a href="mailto:sunny.chen1@unswalumni.com">
+            sunny.chen1@unswalumni.com
+          </a>
+        </p>
+      </div>
     </div>
+  );
+}
+
+function Header1({ content }) {
+  return <h1 className="text-[1.5em]">{content}</h1>;
+}
+
+function ListItemWithBoldPrefix({ bold, normal }) {
+  return (
+    <li>
+      <b>{bold}</b>: {normal}
+    </li>
   );
 }
