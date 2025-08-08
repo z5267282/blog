f369b0af24efaf062634d0a268d97ae366f2f1c6
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-08-08 18:23:45 +1000

    preview cards for projects now work

---
+++ website/src/cards/ProjectPreviewCard.jsx
@@ -0,0 +1,31 @@
+import Header2 from "../components/Header2";
+import Header3 from "../components/Header3";
+import { Link } from "react-router-dom";
+
+export default function ProjectPreviewCard({
+  title,
+  dates,
+  description,
+  technologies,
+}) {
+  return (
+    <Link to="/" className="block w-3/4 min-h-min bg-[#FFE1AF] rounded-lg p-5">
+      <div className="grid grid-cols-[70%_30%]">
+        <b>
+          <Header2 content={title} />
+        </b>
+        <b>
+          <Header2 content={dates} />
+        </b>
+      </div>
+      <b>
+        <Header3 content="Description" />
+      </b>
+      <p>{description}</p>
+      <b>
+        <Header3 content="Technologies" />
+      </b>
+      <p>{technologies}</p>
+    </Link>
+  );
+}
--- website/src/pages/Projects.jsx
+++ website/src/pages/Projects.jsx
@@ -1,3 +1,27 @@
+import Header1 from "../components/Header1";
+
+import ProjectPreviewCard from "../cards/ProjectPreviewCard";
+
 export default function Projects() {
-  return <div>projects</div>
+  return (
+    <div>
+      <div className="flex justify-center items-center h-[calc(1.5em_+20px)] pt-[20px]">
+        <Header1 content="My Projects" />
+      </div>
+      <div className="mt-10 w-full flex flex-col items-center">
+        <FocusTrackerPreview />
+      </div>
+    </div>
+  );
+}
+
+function FocusTrackerPreview() {
+  return (
+    <ProjectPreviewCard
+      title="Focus Tracker"
+      dates="2023"
+      description="An educational tool that executes a Python program and traces significant points in execution. Fith-Year Software Engineering thesis project."
+      technologies="Backend written in Python with unit and integration testing done in Pytest. Frontend written in React JS and vanilla CSS."
+    />
+  );
 }
