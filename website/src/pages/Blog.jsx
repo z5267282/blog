b77046e45e5a8129cf8a8c1ee32f9d9e43d708af
Sunny <z5267282@unsw.edu.au>
2025-08-11 14:35:36 +1000

    blogs fixed

--- website/src/pages/Blog.jsx
+++ website/src/pages/Blog.jsx
@@ -8,11 +8,13 @@ export default function Blog() {
   title = URLtoBlog(title);
 
   return (
-    <>
+    <div className="min-y-screen">
       <title>{title}</title>
-      <header className="text-[2.5em]">{title}</header>
-      {getBlog(lang, title).map((html) => genHTML(html))}
-    </>
+      <header className="text-[1.5em] flex justify-center items-center h-[calc(1.5em_+20px)] w-full pt-[20px]">{title}</header>
+      <div className="w-full h-auto ml-[10vw] mr-[10vw] px-10">
+        {getBlog(lang, title).map((html) => genHTML(html))}
+      </div>
+    </div>
   );
 }
 
