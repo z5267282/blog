dd75073050ca62668f787e20b5ff4ae27b5de5ab
Sunny <z5267282@unsw.edu.au>
2025-08-11 13:48:10 +1000

    blogs are done but need a layout change

--- website/src/pages/LanguageHub.jsx
+++ website/src/pages/LanguageHub.jsx
@@ -2,20 +2,28 @@ import { Link, useParams } from "react-router-dom";
 
 import { getBlogTitlesForLanguage } from "../unpack";
 import { blogToURL } from "../blogToURL";
+import Header1 from "../components/Header1";
 
 export default function LanguageHub() {
   const { lang } = useParams();
 
   return (
-    <>
-      <h1>blogs for {lang}</h1>
-      <ul>
+    <div className="min-h-screen">
+      <div className="flex justify-center items-center h-[calc(1.5em_+20px)] pt-[20px]">
+        <Header1 content={`Language-Semantics for ${lang}`} />
+      </div>
+      <ul className="mt-10 w-full h-auto flex flex-col items-center gap-y-[20px]">
         {getBlogTitlesForLanguage(lang).map((title) => (
-          <li key={`${lang} - ${title}`}>
-            <Link to={`/blogs/${lang}/${blogToURL(title)}`}>{title}</Link>
+          <li className="h-[2em] w-1/3" key={`${lang} - ${title}`}>
+            <Link
+              className="flex justify-center items-center h-full w-full text-[1.em] bg-[#fff4e2] rounded-lg p-5 hover:bg-[#FFE1AF]"
+              to={`/blogs/${lang}/${blogToURL(title)}`}
+            >
+              {title}
+            </Link>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
