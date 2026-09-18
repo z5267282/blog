e6babc9b53aaef4e3cfe3babd79adb4974500426
z5267282 <80458779+z5267282@users.noreply.github.com>
2026-09-18 23:48:37 +1000

    adding files for last commit

---
+++ website/src/routes.js
@@ -0,0 +1,41 @@
+import { getLanguages, getBlogTitlesForLanguage } from "./unpack";
+import { blogToURL } from "./blogToURL";
+import Layout from "./Layout";
+
+import AboutMe from "./pages/AboutMe";
+import ProjectHub from "./pages/ProjectHub";
+import BlogHub from "./pages/BlogHub";
+import LanguageHub from "./pages/LanguageHub";
+import Blog from "./pages/Blog";
+import NotFound from "./pages/NotFound";
+
+const routes = [
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: AboutMe },
+      { path: "projects", Component: ProjectHub },
+      { path: "blogs", Component: BlogHub },
+      {
+        path: "blogs/:lang",
+        Component: LanguageHub,
+        getStaticPaths: () =>
+          Array.from(getLanguages()).map((lang) => `blogs/${lang}`),
+      },
+      {
+        path: "blogs/:lang/:title",
+        Component: Blog,
+        getStaticPaths: () =>
+          Array.from(getLanguages()).flatMap((lang) =>
+            Array.from(getBlogTitlesForLanguage(lang)).map(
+              (title) => `blogs/${lang}/${blogToURL(title)}`
+            )
+          ),
+      },
+      { path: "*", Component: NotFound },
+    ],
+  },
+];
+
+export default routes;
