15956aa6063947e97319145116be202c7b63891e
z5267282 <80458779+z5267282@users.noreply.github.com>
2026-09-18 23:48:04 +1000

    ssg working

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { ViteReactSSG } from "vite-react-ssg";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import "./App.css";
+
+import routes from "./routes";
+
+export const createRoot = ViteReactSSG({
+  routes,
+  basename: import.meta.env.BASE_URL,
+});
